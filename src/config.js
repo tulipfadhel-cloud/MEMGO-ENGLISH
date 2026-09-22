d0eb819e3b329a40c300eff573d2bbf870dc2e30
tulipfadhel-cloud <tulipfadhel@gmail.com>
2026-09-22 17:02:26 +0300

    Build quiz foundation: src/config.js

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const quizConfig = Object.freeze({
+  mode: "assessment",
+  randomizeQuestions: true,
+  randomizeAnswers: true,
+  enableReview: true,
+  persistSession: true,
+  maxNameLength: 60,
+  minImageOptions: 2,
+  storageKey: "memgo.visual-vocabulary.v1"
+});
